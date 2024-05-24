caa6a1faecebd0c9002f2b9e5370e1515ee5435e
hexs <zxjq97@gmail.com>
2024-05-24 14:36:25 +0700

    add libs.json

---
+++ main.py
@@ -0,0 +1,69 @@
+import json
+import subprocess
+import os
+import shutil
+import time
+from pprint import pprint
+# from mdutils.mdutils import MdUtils
+# from mdutils import Html
+from urllib.request import urlopen
+
+
+def get_latest_package_version(package):
+    if type(package) == str:
+        with urlopen(f'https://pypi.org/pypi/{package}/json') as response:
+            data = json.load(response)
+        return data['info']['version']
+
+    with urlopen(f'https://pypi.org/pypi/{package.__name__}/json') as response:
+        data = json.load(response)
+    return data['info']['version']
+
+
+def get_current_package_version(package):
+    return package.__version__
+
+
+def current_is_latest_package_version(package):
+    return get_current_package_version(package) == get_latest_package_version(package)
+
+
+def get_installed_libraries(lib_name):
+    print(f'get_installed_libraries {lib_name}')
+    if 'whl' in os.listdir():
+        shutil.rmtree('whl')
+    os.makedirs('whl', exist_ok=True)
+
+    dir_path = (os.path.dirname(__file__))
+    whl_path = os.path.join(dir_path, 'whl')
+    subprocess.check_output(f'pip download --proxy http://agyc026730:op90-===@150.61.8.70:10080 {lib_name}'
+                            # ['pip', "download", "--proxy", "http://agyc026730:op90-===@150.61.8.70:10080", 'ultralytics']
+                            , cwd=whl_path)
+    library_list = []
+    for lib in os.listdir(whl_path):
+        name = lib.split('-')[0]
+        # name, version, type1, type2, device = lib.split('-')
+        library_list.append(name)
+    return library_list
+
+
+def main():
+    with open('libs.json', encoding='utf-8') as f:
+        libs_dict = json.loads(f.read())
+
+    # use function get_installed_libraries
+    for lib_name, v in libs_dict.items():
+        if v.get('library_list'):
+            continue
+        try:
+            v['library_list'] = get_installed_libraries(lib_name)
+            with open('libs.json', 'w', encoding='utf-8') as file:
+                json.dump(libs_dict, file, ensure_ascii=False, indent=4)
+        except:
+            print(f'{lib_name} error')
+
+    pprint(libs_dict)
+    # with open('libs.json', 'w', encoding='utf-8') as file:
+    #     json.dump(libs_dict, file, ensure_ascii=False, indent=4)
+
+main()
